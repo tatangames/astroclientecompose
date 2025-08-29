def52b730059fa7c59f0df182ea6ed154179f706
Tatan <tatangamess@gmail.com>
2025-08-29 16:29:54 -0600

    version 2.0.0 v1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 27
         targetSdk = 36
         versionCode = 9
-        versionName = "1.1.0"
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
